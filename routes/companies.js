3b6f39714e4fa29f25f2f45877661819d7f8ecbf
Seth Lafollette <sethlafollette@t-Seth-MacBook-Pro.local>
2020-06-29 13:30:04 -0400

    added all companies routes

--- routes/companies.js
+++ routes/companies.js
@@ -50,4 +50,42 @@ companyRoutes.post("/", async (req, res, next) => {
     }
 });
 
+companyRoutes.put("/:code", async (req, res, next) => {
+    try {
+        const { code, name, description } = req.body;
+        const results = await db.query(
+            `UPDATE companies SET name=$2, description=$3
+            WHERE code=$1
+            RETURNING code, name, description`,
+            [code, name, description]
+        );
+        if (results.rowCount === 0) {
+            throw new ExpressError(`Code: "${code}" doesn't exist`, 404);
+        }
+
+        return res.status(200).json({ company: results.rows[0] });
+    }
+    catch (err) {
+        return next(err);
+    }
+});
+
+companyRoutes.delete("/:code", async (req, res, next) => {
+    try {
+        const results = await db.query(
+            `DELETE FROM companies
+            WHERE code=$1`,
+            [req.params.code]
+        );
+        if (results.rowCount === 0) {
+            throw new ExpressError(`Code: "${code}" doesn't exist`, 404);
+        }
+
+        return res.status(200).json({ status: "Deleted"});
+    }
+    catch (err) {
+        return next(err);
+    }
+});
+
 module.exports = companyRoutes;
